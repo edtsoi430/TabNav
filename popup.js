b59a88e58710c5b8300836a1f475e1507eb36f63
paytonmcl <33816540+paytonmcl@users.noreply.github.com>
2019-05-16 20:03:55 -0400

    Add files via upload

--- popup.js
+++ popup.js
@@ -21,22 +21,54 @@
         // Helper function to switch between tabs according to id (in active window)
         function switchTab(tabs_in, id_in){
             chrome.tabs.query({}, function(tabs){
+//                alert(tabs_in[id_in].windowId);
                 chrome.windows.update(tabs_in[id_in].windowId, {focused: true});
                 chrome.tabs.update(tabs_in[id_in].id, {active: true});
             });
         }
-
-
+//currentWindow: true
         chrome.tabs.query({}, function(tabs){        // open search
+//            var x = new String("");
             for(i = 0; i < tabs.length; i++){
+//                x += tabs[i].title + '\n';
+                var img = document.createElement("img")
                 var new_li = document.createElement("li");
                 var new_a = document.createElement('a');
+                var tab = document.createElement("div");
+                var check = document.createElement("input")
+
+                if (tabs[i].favIconUrl) { 
+                    img.setAttribute("src", tabs[i].favIconUrl);
+                    img.setAttribute("width", "20");
+                    img.setAttribute("height", "20");
+                }
+                else {
+                    img.setAttribute("src", "images/bulletpoint.png");
+                    img.setAttribute("width", "15");
+                    img.setAttribute("height", "15");
+                }
+
+                img.setAttribute("alt", "Icon");
+                img.setAttribute("style", "float: left; vertical-align: sub;");
+                    
+                check.setAttribute("type", "checkbox")
+                check.setAttribute("id", "check")
+                check.setAttribute("name", "select")
+                check.setAttribute("style", "float: left; vertical-align: sub;");
+
+
                 new_a.innerHTML = tabs[i].title;
-              //OnClick, executes switch tab function = >passing callback as function argument
+//                Pass callback as function argument
                 new_a.addEventListener("click", switchTab.bind(null, tabs, i));
-              //OnHover, highlight tabs [still testing]
-                //new_a.addEventListener("mouseover", highlight_tabs.bind(null, tabs, i));
-                new_li.appendChild(new_a);
+
+                // if (tabs[i].favIconUrl) {
+                       // new_li.appendChild(img);
+                // }
+
+                tab.innerHTML = check.outerHTML + img.outerHTML + new_a.outerHTML;
+
+                new_li.appendChild(tab);
                 document.getElementById("tabs_results").appendChild(new_li);
             }
+//            alert(x);
         });
