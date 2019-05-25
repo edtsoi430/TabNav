f3799f56e1498a803c45b7499b9e0d2709377ee6
edtsoi <edtsoi430@gmail.com>
2019-05-24 21:33:30 -0400

    fixed # of tabs updating bug

--- background.js
+++ background.js
@@ -1,26 +1,27 @@
 // edtsoi
 
-chrome.declarativeContent.onPageChanged.removeRules(undefined, function() {
-    chrome.declarativeContent.onPageChanged.addRules([{
-    conditions: [new chrome.declarativeContent.PageStateMatcher({})],
-    actions: [new chrome.declarativeContent.ShowPageAction()]
-    }]);
-});
-            chrome.tabs.query({}, function(tabs){
-               chrome.browserAction.setBadgeBackgroundColor({ color: [255, 0, 0, 0] });
-               chrome.browserAction.setBadgeText({text: tabs.length.toString()});
-            });
-        
-        function updateBadge(){
-            chrome.tabs.query({}, function(tabs){
-               chrome.browserAction.setBadgeBackgroundColor({ color: [255, 0, 0, 0] });
-               chrome.browserAction.setBadgeText({text: tabs.length.toString()});
-            });
-        }
+    chrome.declarativeContent.onPageChanged.removeRules(undefined, function() {
+        chrome.declarativeContent.onPageChanged.addRules([{
+        conditions: [new chrome.declarativeContent.PageStateMatcher({})],
+        actions: [new chrome.declarativeContent.ShowPageAction()]
+        }]);
+    });
 
-        chrome.tabs.onUpdated.addListener(updateBadge.bind());
-        chrome.tabs.onRemoved.addListener(updateBadge.bind());
-        chrome.tabs.onCreated.addListener(updateBadge.bind());
+    chrome.tabs.query({}, function(tabs){
+        chrome.browserAction.setBadgeBackgroundColor({ color: [255, 0, 0, 0] });
+        chrome.browserAction.setBadgeText({text: tabs.length.toString()});
+    });
+        
+    function updateBadge(){
+        chrome.tabs.query({}, function(tabs){
+        chrome.browserAction.setBadgeBackgroundColor({ color: [255, 0, 0, 0] });
+        chrome.browserAction.setBadgeText({text: tabs.length.toString()});
+        });
+    }
+
+    chrome.tabs.onUpdated.addListener(updateBadge.bind());
+    chrome.tabs.onRemoved.addListener(updateBadge.bind());
+    chrome.tabs.onCreated.addListener(updateBadge.bind());
 
 //chrome.commands.onCommand.addListener(function(command) {
 //  chrome.tabs.query({currentWindow: true}, function(tabs) {
