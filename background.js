146cdb1b6c5631ec228293a96ea7f0cbce6e35fe
zww <zww@umich.edu>
2019-06-10 16:21:46 -0400

    update

--- background.js
+++ background.js
@@ -1,13 +1,28 @@
 // edtsoi
 
-chrome.declarativeContent.onPageChanged.removeRules(undefined, function() {
-    chrome.declarativeContent.onPageChanged.addRules([{
-    conditions: [new chrome.declarativeContent.PageStateMatcher({})],
-    actions: [new chrome.declarativeContent.ShowPageAction()]
-    }]);
-});
+    chrome.declarativeContent.onPageChanged.removeRules(undefined, function() {
+        chrome.declarativeContent.onPageChanged.addRules([{
+        conditions: [new chrome.declarativeContent.PageStateMatcher({})],
+        actions: [new chrome.declarativeContent.ShowPageAction()]
+        }]);
+    });
 
-// Template in case extra commands need to be done
+    chrome.tabs.query({}, function(tabs){
+        chrome.browserAction.setBadgeBackgroundColor({ color: [255, 0, 0, 0] });
+        chrome.browserAction.setBadgeText({text: tabs.length.toString()});
+    });
+        
+    function updateBadge(){
+        chrome.tabs.query({}, function(tabs){
+        chrome.browserAction.setBadgeBackgroundColor({ color: [255, 0, 0, 0] });
+        chrome.browserAction.setBadgeText({text: tabs.length.toString()});
+        });
+    }
+
+    chrome.tabs.onUpdated.addListener(updateBadge.bind());
+    chrome.tabs.onRemoved.addListener(updateBadge.bind());
+//    chrome.tabs.onRemoved.addListener(updateTabResults.bind());
+    chrome.tabs.onCreated.addListener(updateBadge.bind());
 
 //chrome.commands.onCommand.addListener(function(command) {
 //  chrome.tabs.query({currentWindow: true}, function(tabs) {
--- popup.js
+++ popup.js
@@ -26,18 +26,192 @@
                 chrome.tabs.update(tabs_in[id_in].id, {active: true});
             });
         }
-//currentWindow: true
-        chrome.tabs.query({}, function(tabs){        // open search
-//            var x = new String("");
-            for(i = 0; i < tabs.length; i++){
-//                x += tabs[i].title + '\n';
-                var new_li = document.createElement("li");
-                var new_a = document.createElement('a');
-                new_a.innerHTML = tabs[i].title;
-//                Pass callback as function argument
-                new_a.addEventListener("click", switchTab.bind(null, tabs, i));
-                new_li.appendChild(new_a);
-                document.getElementById("tabs_results").appendChild(new_li);
-            }
-//            alert(x);
+
+
+        function closeTab(tabs_in, id_in, cur_a, cur_img){
+            chrome.tabs.query({}, function(tabs){
+                chrome.tabs.remove(tabs_in[id_in].id);
+            });
+            cur_a.remove();
+            cur_img.remove();
+            //location.reload();
+            // add in code that adjust height of popup upon closing
+            event.stopPropagation();
+        }
+
+        // Helper for parsing html. Counting occurence.
+        function count(string,char) {
+          var re = new RegExp(char,"gi");
+          return string.match(re).length;
+        }
+
+        //return promise using asynchronous query
+        function get_cur_tab_id(){
+          var promise = new Promise(function(resolve, reject){
+            chrome.tabs.query({highlighted: true, lastFocusedWindow: true}, function(tabs){
+               resolve(tabs[0]);
+            });
+          })
+          return promise;
+        };
+
+        //chain the promise to solve asynchronous problem
+        var cur_tab;
+        get_cur_tab_id().then(function(result){
+           cur_tab= result;
         });
+        
+        // Helper function to create new window rows in popup (for drag and drop use)
+        function newWindow(){
+            chrome.windows.getAll({populate: true,}, function(windows){
+                console.log(tabsToMove.size);
+                if (tabsToMove.length == 0) {
+                    chrome.windows.create({type: "normal"});
+                }
+                else {
+                    var windowId_1;
+                    var window1Index = Number(tabsToMove[0].split(' ')[0]);
+                    var tab1Index = Number(tabsToMove[0].split(' ')[1]);
+                    chrome.windows.create({type: "normal",tabId: windows[window1Index].tabs[tab1Index].id}, function(window){
+                        windowId_1 = window.id;
+                        chrome.tabs.query({}, function(tabs){
+                            var list = [];
+                            for(k = 1; k < tabsToMove.length; k++) {
+                                var windowIndex = Number(tabsToMove[k].split(' ')[0]);
+                                var tabIndex = Number(tabsToMove[k].split(' ')[1]);
+                                list.push(windows[windowIndex].tabs[tabIndex].id);
+
+                            }
+                            chrome.tabs.move(list, {windowId : windowId_1, index: -1});
+                            tabsToMove = [];
+                        });
+                    });
+                }
+                location.reload();
+            });
+        }
+
+        document.getElementById("new-window").addEventListener("click", newWindow);
+
+    //--------------------------------------
+    var tabsToMove = [];
+    function updateTabResults(){
+        chrome.windows.getAll({populate: true,}, function(windows){
+            for(i = 0; i < windows.length; i++){
+                var title = document.createElement('div');
+                title.setAttribute("class", "window");
+                title.setAttribute("id", i);
+                title.innerHTML = "Window " + (i + 1).toString();
+                title.addEventListener("click", function(e) {
+                        if (!tabsToMove.size == 0) {
+                            var window1Index = Number(tabsToMove[0].split(' ')[0]);
+                            var tab1Index = Number(tabsToMove[0].split(' ')[1]);
+
+                            chrome.tabs.query({}, function(tabs){
+                                var list = [];
+                                for(k = 0; k < tabsToMove.length; k++) {
+                                    var windowIndex = Number(tabsToMove[k].split(' ')[0]);
+                                    var tabIndex = Number(tabsToMove[k].split(' ')[1]);
+                                    list.push(windows[windowIndex].tabs[tabIndex].id);
+                                    //console.log(windows[e.path[1].id.split(' ')[0]]);
+                                    console.log(e.path[1].id);
+                                }
+                                chrome.tabs.move(list, {windowId : windows[Number(e.path[1].id)].id, index: -1});
+                                tabsToMove = [];
+                            });
+                        }
+                        console.log(e.path[1].id);
+                        console.log(windows[Number(e.path[1].id)].tabs);
+                        // switchTab.bind(null, windows[Number(e.path[1].id)].tabs, 0);
+
+                    }, false);
+                
+                document.getElementById("tabs_results").appendChild(title);
+                for(j = 0; j < windows[i].tabs.length; j++){
+                    var img = document.createElement("img")
+                    var new_li = document.createElement("li");
+                    var new_a = document.createElement('a');
+                    var x = document.createElement("button");
+                    var span = document.createElement("span");
+
+                    new_li.setAttribute("id", i + " " + j);
+                    new_a.setAttribute("id", i + " " + j);
+                    span.setAttribute("id", i + " " + j);
+                    img.setAttribute("id", i + " " + j);
+
+                    if (windows[i].tabs[j].favIconUrl) {
+                        img.setAttribute("src", windows[i].tabs[j].favIconUrl);
+                    }
+                    else {
+                        img.setAttribute("src", "images/bulletpoint.png");
+                    }
+                    img.width = img.height = 30;
+                    img.setAttribute("style", "float: left; vertical-align: middle;");
+                    img.setAttribute("class", "favicon");
+                    span.setAttribute("aria-hidden", "true");
+                    span.innerHTML = "&times;";
+                    
+                    x.className="closeSpan";
+                    x.setAttribute("type", "button");
+                    x.setAttribute("class", "close");
+                    x.setAttribute("aria-label", "close");
+                    x.setAttribute("style", "float: right; vertical-align: middle;");
+                    x.width = x.height = 15;
+                    x.appendChild(span);
+                    x.addEventListener("click", closeTab.bind(null, windows[i].tabs, j, new_a, img));
+
+                    //used span to avoid two hyperlinks.
+                    let name = document.createElement("span");
+                    let url = document.createElement("span");
+                    if(windows[i].tabs[j].title.length > 35){
+                      name.innerHTML = windows[i].tabs[j].title.substring(0,35) +'...' + "<br />";
+                    }
+                    else{
+                      name.innerHTML = windows[i].tabs[j].title +"<br />";
+                    }
+
+                    name.setAttribute("style", "font-size: 80%;");
+                    url.setAttribute("style", "color: grey; font-size: 60%;");
+
+                    //parse address before the third slash.
+                    if(count(windows[i].tabs[j].url.substring(0, windows[i].tabs[j].url.length-1), '/') <= 2){
+                      url.innerHTML = windows[i].tabs[j].url;
+                    }
+                    else{
+                      url.innerHTML =  windows[i].tabs[j].url.substring(0, windows[i].tabs[j].url.indexOf('/', 8));
+                    }
+
+                    new_a.appendChild(name);
+                    new_a.appendChild(url);
+                    new_a.appendChild(x);
+                    new_a.setAttribute("draggable", true);
+
+                    //cur_tab from get_cur_tab_id. Used promise to solve asynchronous problem.
+                    if(windows[i].tabs[j].id == cur_tab.id){
+                      new_a.setAttribute("style", "background-color: #A7E8FF;");
+                    };
+                    new_li.appendChild(img);
+                    new_li.appendChild(new_a);
+                    new_li.addEventListener('contextmenu', function(e) {
+                        var tab = e.path[1].id;
+                        if (tabsToMove.includes(tab)){
+                            tabsToMove.splice(tabsToMove.indexOf(tab), 1);
+                            this.style.backgroundColor = 'transparent';
+                            var aCol = this.getElementsByTagName( 'a' );
+                            aCol[0].style.backgroundColor = 'transparent';
+                        }
+                        else {
+                            tabsToMove.push(tab);
+                            this.style.backgroundColor = 'red';
+                            var aCol = this.getElementsByTagName( 'a' );
+                            aCol[0].style.backgroundColor = '#F9B7E1';
+                        }
+                        e.preventDefault();
+                    }, false);
+                    new_li.addEventListener("click", switchTab.bind(null, windows[i].tabs, j));
+                    document.getElementById("tabs_results").appendChild(new_li);
+                }
+            }
+        });
+    }
+    updateTabResults();
